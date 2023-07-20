d35e8a80052efe9de48c7cd5a5924d72e1a54f1e
David Olona <damilear@outlook.com>
2023-07-19 21:11:47 -0400

    Track scores added

--- rps.js
+++ rps.js
@@ -3,6 +3,9 @@ let options = ['Rock',
 'Paper',
 'Scissors'];
 
+let playerScore = 0;
+let cpuScore = 0;
+
 // getComputer Choice
 function getComputerChoice() {
 
@@ -25,22 +28,21 @@ function playRound(e) {
     let lowerCaseComputerChoice = getComputerChoice().toLowerCase();
     const results = document.getElementById('results');
     
-    // Compare selections
     
     // player selects Rock
     if (lowerCasePlayerChoice === options[0].toLowerCase()) {
         if (lowerCaseComputerChoice === options[1].toLowerCase()) {
             // console.log('You Lose! Paper beats Rock');
             results.textContent = 'You Lose! Paper beats Rock';
-            return 1;
+            cpuScore += 1;
         } else if (lowerCaseComputerChoice === options[2].toLowerCase()) {
             // console.log('You Win! Rock beats Scissors');
             results.textContent = 'You Win! Rock beats Scissors'
-            return 2;
+            playerScore += 1
         } else {
             // console.log('It\'s a Draw! We both chose Rock');
             results.textContent = 'It\'s a Draw! We both chose Rock'
-            return 0;
+            return ;
         }
     }
 
@@ -48,13 +50,13 @@ function playRound(e) {
     if (lowerCasePlayerChoice === options[1].toLowerCase()) {
         if (lowerCaseComputerChoice === options[2].toLowerCase()) {
             results.textContent = 'You Lose! Scissors beats Paper';
-            return 1;
+            cpuScore += 1;
         } else if (lowerCaseComputerChoice === options[0].toLowerCase()) {
             results.textContent = 'You Win! Paper beats Rock';
-            return 2;
+            playerScore += 1
         } else {
             results.textContent = 'It\'s a Draw! We both chose Paper';
-            return 0;
+            return ;
         }
     }
 
@@ -62,15 +64,29 @@ function playRound(e) {
     if (lowerCasePlayerChoice === options[2].toLowerCase()) {
         if (lowerCaseComputerChoice === options[0].toLowerCase()) {
             results.textContent = 'You Lose! Rock beats Scissors';
-            return 1;
+            cpuScore += 1;
         } else if (lowerCaseComputerChoice === options[1].toLowerCase()) {
             results.textContent = 'You Win! Scissors beats Paper';
-            return 2;
+            playerScore += 1
         } else {
             results.textContent = 'It\'s a Draw! We both chose Scissors';
-            return 0;
+            return ;
         }
     }
+
+    // Check for score
+    if (playerScore === 5) {
+        results.textContent = 'You Win!';
+        playerScore = 0;
+        cpuScore = 0;
+    }
+    if  (cpuScore === 5) {
+        results.textContent = 'You Lose! Computer Wins! :(';
+        playerScore = 0;
+        cpuScore = 0;
+    }
+    
+    console.log(`The score is ${playerScore}||${cpuScore}`);
 
 } 
 
@@ -81,65 +97,65 @@ function playRound(e) {
 // -----------------------------------------------------------
 
 // game function
-function playGame() {
-    // varaible for player and computer score
-    let playerScore = 0;
-    let computerScore = 0;
+// function playGame() {
+//     // varaible for player and computer score
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    // variable for max rounds and current round
-    const maxRounds = 5;
-    let currentRound = 1;
+//     // variable for max rounds and current round
+//     const maxRounds = 5;
+//     let currentRound = 1;
 
-    // loop until curr round > max rounds and update scores
-    while(currentRound <= maxRounds) {
+//     // loop until curr round > max rounds and update scores
+//     while(currentRound <= maxRounds) {
 
-        // player choice
-        let playerChoice = prompt('Rock, Paper or Scissors?');
-        console.log('choice at line 94 ' + playerChoice);
-        playerChoice = playerChoice.toLowerCase();
-        console.log('choice at line 96 ' + playerChoice);
+//         // player choice
+//         let playerChoice = prompt('Rock, Paper or Scissors?');
+//         console.log('choice at line 94 ' + playerChoice);
+//         playerChoice = playerChoice.toLowerCase();
+//         console.log('choice at line 96 ' + playerChoice);
 
-        // handle invalid choice
-        while (playerChoice !== 'rock' && playerChoice !== 'paper' && 
-            playerChoice !== 'scissors') {
-            console.log('choice at line 100 ' + playerChoice);
-            playerChoice = prompt('Try again').toLowerCase();
-            console.log('choice at line 102 ' + playerChoice);
-        }
+//         // handle invalid choice
+//         while (playerChoice !== 'rock' && playerChoice !== 'paper' && 
+//             playerChoice !== 'scissors') {
+//             console.log('choice at line 100 ' + playerChoice);
+//             playerChoice = prompt('Try again').toLowerCase();
+//             console.log('choice at line 102 ' + playerChoice);
+//         }
 
-        // cpu choice
-        let computerChoice = getComputerChoice();
+//         // cpu choice
+//         let computerChoice = getComputerChoice();
 
-        // Play the round
-        let roundResult = playRound(playerChoice, computerChoice);
+//         // Play the round
+//         let roundResult = playRound(playerChoice, computerChoice);
 
-        // Update Scores
-        if (roundResult === 1) {
-            computerScore += 1;
-            console.log(`Player wins round ${currentRound}!`);
-        } else if (roundResult === 2) {
-            playerScore += 1;
-            console.log(`Computer wins round ${currentRound}!`);
-        } else {
-            playerScore += 1;
-            computerScore += 1;
-            console.log(`Nobody wins round ${currentRound} it was a draw!`);
-        }
+//         // Update Scores
+//         if (roundResult === 1) {
+//             computerScore += 1;
+//             console.log(`Player wins round ${currentRound}!`);
+//         } else if (roundResult === 2) {
+//             playerScore += 1;
+//             console.log(`Computer wins round ${currentRound}!`);
+//         } else {
+//             playerScore += 1;
+//             computerScore += 1;
+//             console.log(`Nobody wins round ${currentRound} it was a draw!`);
+//         }
 
-        // Update Round
-        currentRound += 1;
-    }
+//         // Update Round
+//         currentRound += 1;
+//     }
 
-    // return winner
-    if (playerScore > computerScore) {
-        console.log('Player Wins!');
-    } else if (playerScore < computerScore) {
-        console.log('Computer Wins!');
-    } else {
-        console.log('Sorry it was a draw!');
-    }
+//     // return winner
+//     if (playerScore > computerScore) {
+//         console.log('Player Wins!');
+//     } else if (playerScore < computerScore) {
+//         console.log('Computer Wins!');
+//     } else {
+//         console.log('Sorry it was a draw!');
+//     }
     
-}
+// }
 
 const rock = document.getElementById('rockButton');
 const paper = document.getElementById('paperButton');
